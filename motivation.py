00efb7f1b1852544b1d75d3f23edbca04e8dfbde
10742943-hash <10742943@suzannecoryhs.vic.edu.au>
2026-08-13 13:56:55 +1000

    do-not-run-this-code

---
+++ motivation.py
@@ -0,0 +1,19 @@
+import random
+
+messages = [
+    "Your code isn't broken. It's just exploring alternative outcomes.",
+    "There is no bug. Only an undocumented feature.",
+    "Have you tried printing the variable?",
+    "One does not simply write code without debugging.",
+    "If it works first time, be suspicious.",
+    "99 little bugs in the code...",
+    "Today's objective: make the error message different.",
+    "Computers do exactly what you tell them. Unfortunately.",
+    "Ctrl + Z is not a debugging strategy.",
+    "The best debugger is still print()."
+
+]
+
+print("\n💻 MESSAGE FROM YOUR REPOSITORY\n")
+print(random.choice(messages))
+print()
