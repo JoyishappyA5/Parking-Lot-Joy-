9d814fea4e51ceff5042698fee958dcbb0ea9ae0
JoyishappyA5 <joygong6@gmail.com>
2026-08-13 12:46:41 +1000

    HoW Old

--- Test.py
+++ Test.py
@@ -1,1 +1,8 @@
 print("hello world")
+age = int(input("How old are you?"))
+if age>15:
+    print("You are older than me")
+elif age == 15:
+    print("You are at the best age")
+else:
+    print("Kid! Ha")
